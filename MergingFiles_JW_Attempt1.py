9dbdca286a47f9594531e8ad585a6eecec36d4fa
Josephkworden <128114509+Josephkworden@users.noreply.github.com>
2023-04-26 20:05:37 -0600

    Add files via upload

---
+++ MergingFiles_JW_Attempt1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Importing pandas and numpy
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+#Reading in patient file
+patient = pd.read_csv("patient.csv")
+patient
+
+
+# In[3]:
+
+
+#Reading in diagnosis file
+diagnosis = pd.read_csv("diagnosis.csv")
+diagnosis
+
+
+# In[4]:
+
+
+#Merge patient file with diagnosis file
+ChronicCoughDF1 = pd.merge(patient, diagnosis, how='inner', left_on = 'patient_id', right_on = 'patient_id')
+ChronicCoughDF1
+
+
+# In[6]:
+
+
+#Dropping unneccesary columns in the ChronicCough file
+    #These include "derived_by_TriNetX", "reason_yob_missing", "death_date_source_id", "source_id_x", "source_id_y"
+ChronicCoughDF2 = ChronicCoughDF1.drop(columns=["derived_by_TriNetX", "reason_yob_missing", 
+                                                "death_date_source_id","source_id_x", "source_id_y"])
+ChronicCoughDF2
+
+
+# In[ ]:
+
+
+#Creating the new variable documenting the number of cough diagnosis per patient
+ChronicCough = ChronicCoughDF2.assign(cough_dx=)
+
